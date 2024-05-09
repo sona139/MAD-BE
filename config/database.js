dfa5d8c65a32cfe69698509edb6ac281f0d58683
sonbh <sonbh@bravebits.vn>
2024-05-09 13:27:27 +0700

    Update app

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 require("dotenv/config");
 
-module.exports = new Sequelize('cashflow', 'root', '', {
+module.exports = new Sequelize('cashflow', 'root', 'Iuhuyennhat1-', {
   host: '127.0.0.1',
   dialect: 'mysql',
 });
